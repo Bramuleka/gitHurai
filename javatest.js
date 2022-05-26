1da17a8fd64bc084ecdb5c3c76b6cb075dfc98b1
wa773nbad3z3 <33803067+wa773nbad3z3@users.noreply.github.com>
2022-05-26 06:23:48 +0100

    Add files via upload

---
+++ javatest.js
@@ -0,0 +1,22 @@
+var cities = [
+    "Harare",
+    "Lagos",
+    "Mutare",
+    "Mutoko",
+
+
+];
+
+var rankcity = Function (city){
+
+    for (var i=0;i<cities.lenth;i++){
+        if (cities[I===city]){
+            return city + "is mumber"+(i+1);
+        };
+    };
+
+    return city = "is not among the top 5";
+};
+
+console.log(rankcity("Harare"));
+console.log(rankcity("Mutoko"));
---
+++ javc.js
@@ -0,0 +1,8 @@
+function printName(name){ 
+    console.log('My name is :'+ name.toUpperCase());
+
+
+}
+
+var publish = printName;
+printName("Warren");
